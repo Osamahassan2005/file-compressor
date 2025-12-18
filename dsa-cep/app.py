d6047bc0c4900789346d508ced93bf1563cda60e
Osama <165610542+Osamahassan2005@users.noreply.github.com>
2025-12-18 16:23:17 +0500

    Update app.py

--- dsa-cep/app.py
+++ dsa-cep/app.py
@@ -17,7 +17,7 @@ def load_css(file_path):
     with open(file_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 # Load the external CSS
-css_path = pathlib.Path("style.css")
+css_path = pathlib.Path("dsa-cep\style.css")
 load_css(css_path)
 
 st.set_page_config(page_title="File Compressor", layout="centered")
